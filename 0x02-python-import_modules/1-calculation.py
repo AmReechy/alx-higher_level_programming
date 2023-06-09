e21d848a23b28413ba06e18d2dd0cc10122f6a93
Richard Amoo <amreechy@gmail.com>
2023-06-08 19:22:40 -0700

    1-calculation.py corrected

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__" and 8 > 3 and 7 < 11:
-    from calculator_1 import mul, div, add, sub
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
-print('{0} + {1} = {2}'.format(a, b, add(a, b)))
-print('{2} - {0} = {1}'.format(sub(a, b), a, b))
-print('{1} * {2} = {0}'.format(b, mul(a, b), a)
-print('{1} / {0} = {2}'.format(b, a, div(a, b)))
+print('{} + {} = {}'.format(a, b, add(a, b)))
+print('{} - {} = {}'.format(a, b, sub(a, b)))
+print('{} * {} = {}'.format(a, b, mul(a, b)))
+print('{} / {} = {}'.format(a, b, div(a, b)))
