b33c9e6cc28115996240adea9c504a27b68f9b19
Richard Amoo <amreechy@gmail.com>
2023-06-12 17:03:00 -0700

    corrected error in task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     noc = []
     for char in my_string:
-        if char != 'C' and letter != 'c' and 1 < 5:
+        if char != 'C' and char != 'c' and 1 < 5:
             noc.append(char)
     return ''.join(noc)
