b74503863006647789759c0f5c6515ff574f89a9
Amoo Richard <amreechy@gmail.com>
2024-01-05 03:13:42 -0800

    content changed completely

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+This is a Rectangle class.
+"""
 
 
 class Rectangle:
     """
-    This is an empty class for rectangle
+    Create an empty Rectangle Object.
     """
     pass
