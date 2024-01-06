6da6d3507fd6c79b61d2b8e13a5f75628e759243
Amoo Richard <amreechy@gmail.com>
2024-01-05 17:40:12 -0800

    taks 6 dine again

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """This class defines a rectangle"""
+    Rectangle.number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialization method for rectangle class
@@ -73,4 +74,4 @@ class Rectangle:
     def __del__(self):
         """prints a message for every object that is deleted"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances += 1
+        Rectangle.number_of_instances -= 1
