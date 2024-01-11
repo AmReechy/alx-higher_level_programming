e59cdf9c4a7ba51b0c98aa3c138f27540e2a5b7f
Amoo Richard <amreechy@gmail.com>
2024-01-10 16:24:31 -0800

    task4 first attempt

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with function that checks if
+an object is a subclass of a specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass
+    of a_class, False otherwise
+    """
+    return issubclass(obj,a_class)
